7153c53a10d707b23251f401e6268f5e56476100
Albert Zak <me@albertzak.com>
2017-04-08 00:21:05 +0200

    Add basic distillery config

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Relay.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 1.0"}]
+     {:plug, "~> 1.0"},
+     {:distillery, "~> 1.0"}]
   end
 end
